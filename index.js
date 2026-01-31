8656d8fb2e1e02f3ea6baa3465696fc69d4dd29b
Maria Fructuoso <marifructuosog718@gmail.com>
2026-01-31 21:49:37 +0100

    Parte 2.2 terminada

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 import mongoose from 'mongoose';
 import express from 'express';
+import nunjucks from 'nunjucks';
+
 
 import authRoutes from './routes/auth.js';
-
-
-mongoose.connect('mongodb://localhost:27017/basketleaguemanager');
-
 import playerRoutes from './routes/players.js';
 import teamRoutes from './routes/teams.js';
 import matchRoutes from './routes/matches.js'; 
 
+
+mongoose.connect('mongodb://localhost:27017/basketleaguemanager');
+
+
+
 const app = express();
 
+app.set('view engine', 'njk');
+nunjucks.configure('views', {
+autoescape: true,
+express: app
+});
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//configurar el nunjucks con import/export
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
+app.use('/public', express.static('./public'));
 
 app.use('/players', playerRoutes); 
 app.use('/teams', teamRoutes);
